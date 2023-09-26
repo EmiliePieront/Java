0edf014ee674f29983fed653898dbcca4a9e68b4
Emilie <pieront.emilie@gmail.com>
2023-09-26 20:55:48 +0200

    Deuxieme cours

--- ToucheCoule/Programme.java
+++ ToucheCoule/Programme.java
@@ -1,5 +1,7 @@
 package ToucheCoule;
 
+import java.util.Arrays;
+
 public class Programme {
     public static void main(String[] args) {
         System.out.println("Hello world !");
@@ -14,6 +16,59 @@ public class Programme {
         //Creation of a table for the battle grid
         var attackGrid = new char[100]; //Creation and initialisation of a table
         int[] integerTab = {1,2,3,4,5,6,7};
+        //creation of a characters table
+        var boatsGrid = new char[100];
+        //show an element of the table
+        System.out.println("args = " + attackGrid[0]);
+        //filling attack grid with "."
 
+        for (int i = 0, y = 0; i < attackGrid.length ; i++, y++){
+            //U can do this way
+            attackGrid[i] = '.';
+        }
+        //Or This way, to fill the grid.
+        Arrays.fill(boatsGrid, '.');
+
+
+        //Display of the attackGrid by columns
+        System.out.println("Attack grid");
+        displayGrid(attackGrid);
+
+
+        /*
+        var a = 0;
+        var b = 0;
+        a = b++;
+        System.out.println(a+" et "+b);
+        a = 0; b = 0;
+        a = ++b;
+        System.out.println(a+" et "+b);
+        System.out.println("args = " + attackGrid[0]);
+        System.out.println(Arrays.toString(boatsGrid));
+        */
+        System.out.println("Boats grid");
+        displayGrid(boatsGrid);
+    }
+
+    /**
+     * display a grid with header
+     * @param table is the table to display
+     */
+    static void displayGrid (char[] table){
+        //Display of the BoatsGrid by columns
+        System.out.print("\t");
+        for (char lettre = 'A'; lettre < 'A'+10; System.out.print(lettre+" "), lettre++);
+        System.out.println();
+        for (var i=0; i < table.length; i++){
+            if(i%10 == 0){
+//                if(i < 90) System.out.print(" ");
+//                System.out.print(i/10+1);
+                System.out.printf(" %2d ", i/10+1);
+            }
+            System.out.print(table[i]+" ");
+            if(i%10 ==9){
+                System.out.println();
+            }
+        }
     }
 }
